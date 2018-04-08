6da15d4ef141ac4ebbaf2b0dd68a09fe57a3598c
Isaac Lyman <isaaclyman@gmail.com>
2018-04-08 08:11:03 -0600

    Target UMD in webpack build and add 'main' property to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,24 @@ const path = require('path');
 
 module.exports = {
     entry: {
-      "blackswan": "./source/base.ts",
-      "example": "./examples/example-player.js"
+      'blackswan': './source/base.ts',
+      'example': './examples/example-player.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "[name].js"
+        filename: '[name].js',
+        library: 'blackswan',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
     },
     resolve: {
         // Add '.ts' and '.tsx' as a resolvable extension.
-        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
     },
     module: {
         rules: [
             // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            { test: /\.tsx?$/, loader: 'ts-loader' }
         ]
     }
 }
